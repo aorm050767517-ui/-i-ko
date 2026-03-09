2a96ce3155cc852413ddd3069b4c86cb62a0bcf2
aorm050767517-ui <aorm050767517@gmail.com>
2026-03-09 04:32:02 +0300

    Create homepage with animated typing text and gradient background

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; // Make sure to create this file for styling
+
+const HomePage = () => {
+    return (
+        <div className="gradient-background">
+            <h1 className="typing-text">🍝 Welcome to Şişko Makarna - Delicious Pasta Awaits You! 🍝</h1>
+        </div>
+    );
+};
+
+export default HomePage;
