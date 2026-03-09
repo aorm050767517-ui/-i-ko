fb13b0c21dcb9be62abfae704f579ae8622078d9
aorm050767517-ui <aorm050767517@gmail.com>
2026-03-09 04:37:22 +0300

    Create app/layout.tsx with Şişko Makarna metadata and pasta emoji favicon

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+<head>
+    <title>Şişko Makarna</title>
+    <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAIAAAjQLeKAAAAAAXRFWHRTb2Z0d2FyZQBNYXRwbG90bGliIDIuMS4zMzk1MDg1NzY0Y2U0OcIHB16EAAACHyApJ6ZmZ71tDGTRDJo2dLT3b7NDuL45V1HvBm5pZQR3zz+Hvs0BkJd9MbaWbD/gkAAAACXZwQ17p6dUMAAABxSURBVFhH7ZUBDoAwDPp69/OwAj9vsJVgnQ0mMKQAGAZQ8Q4pEW3SH2yi45TynxWOCgAAAABJRU5ErkJggg==" type="image/png">
+</head>
+<body>
+    <h1>Welcome to Şişko Makarna</h1>
+    <p>Enjoy your pasta! 🍝</p>
+</body>
